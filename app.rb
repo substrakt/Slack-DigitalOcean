5d7f7d16aa5e0f3faa4d53ef54cbd0a0622cdb31
Max Woolf <max@woolf.io>
2015-02-26 15:10:14 +0000

    more things

--- app.rb
+++ app.rb
@@ -4,14 +4,28 @@ require 'barge'
 
 post '/restartserver' do
   @barge = Barge::Client.new(access_token: ENV['DIGITAL_OCEAN_KEY'])
-  show_help if params['text'].nil? or params['text'].empty?
+  if params['text'].nil? or params['text'].empty?
+    show_help
+  else
+    restart_server(params['text'].to_i)
+  end
 end
+
+private
 
 def show_help
   response = ":bangbang: No server selected. Choose one of these... \n"
   @barge.droplet.all.response.body.droplets.each do |droplet|
-    response << "- `/restartserver #{ droplet.id }` for #{ droplet.name } \n"
+    response << "`/restartserver #{ droplet.id }` for #{ droplet.name } \n"
   end
 
   response
+end
+
+def restart_server(droplet_id)
+  if @barge.droplet.power_cycle(droplet_id).success?
+    "Restarting server #{ droplet_id } :clock2:"
+  else
+    "Failed to restart. :confused:"
+  end
 end
